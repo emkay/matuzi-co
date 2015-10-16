e0074343be411f7fdc5cacd43cd9077229915653
Michael Matuzak <mmatuzak@gmail.com>
2015-10-16 13:41:10 -0700

    bleh

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var domready = require('domready')
 var text3dParticles = require('text-3d-particles')
 
-var opts = 
-    { width: 800
-    , height: 400
-    , fontSize: 100
-    , density: 4
-    , text: 'Matuzi Co'
-    , foreground: '#111'
-    , background: '#fff'
-    , duration: 6000
+var opts = {
+    width: 800,
+    height: 400,
+    fontSize: 100,
+    density: 4,
+    text: 'Matuzi Co',
+    foreground: '#111',
+    background: '#fff',
+    duration: 6000,
 }
 
 var textGraph = text3dParticles(opts, function() {})
